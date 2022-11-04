dcae4e0a10fb86ec2097f02fa2f51117dbf438b8
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-04 09:12:07 -0700

    Initial commit

---
+++ ships.js
@@ -0,0 +1,40 @@
+let ship1 = {
+
+    name: 'H.M.S. Ferdinand II',
+    class: 'Retion',
+    type: 'Cruiser',
+    shipyard: 'Mars Orbit',
+    yearBuilt: 2472,
+    alignment: 'EDF',
+    faction: null,
+
+    engines: {
+        count: 6,
+        make: 'Medin Industries',
+        model: 'Gibraltar',
+        features: {
+            warpDrive: true,
+            slipSpace: false
+        }
+    },
+
+    crew: {
+        capacity: 25,
+        captain: {
+            name: 'Geland Frode',
+            age: 36
+        },
+        navigator: {
+            name: 'Johan Dermio',
+            age: 27
+        },
+        engineer: {
+            name: 'Relana Regara',
+            age: 25
+        }
+    },
+
+    armament: {
+
+    }
+}
