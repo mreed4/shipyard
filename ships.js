27c3a4f97bbcc1350e4b8b8c12d1b8e173f0108f
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-04 09:13:53 -0700

    Begin writing arrays to later randomize

--- ships.js
+++ ships.js
@@ -1,40 +1,45 @@
-let ship1 = {
+let shipClass = ["Retion", "Varret", "Donbas", "Gesan", "Hyperion"];
 
-    name: 'H.M.S. Ferdinand II',
-    class: 'Retion',
-    type: 'Cruiser',
-    shipyard: 'Mars Orbit',
-    yearBuilt: 2472,
-    alignment: 'EDF',
-    faction: null,
-
-    engines: {
-        count: 6,
-        make: 'Medin Industries',
-        model: 'Gibraltar',
-        features: {
-            warpDrive: true,
-            slipSpace: false
-        }
-    },
-
-    crew: {
-        capacity: 25,
-        captain: {
-            name: 'Geland Frode',
-            age: 36
-        },
-        navigator: {
-            name: 'Johan Dermio',
-            age: 27
-        },
-        engineer: {
-            name: 'Relana Regara',
-            age: 25
-        }
-    },
-
-    armament: {
-
-    }
+class Ship {
+  constructor() {}
 }
+
+let ship1 = {
+  general: {
+    name: "H.M.S. Ferdinand II",
+    class: "Retion",
+    type: "Cruiser",
+    shipyard: "Mars Orbit",
+    yearBuilt: 2472,
+    alignment: "EDF",
+    faction: null,
+  },
+
+  engines: {
+    count: 6,
+    make: "Medin Industries",
+    model: "Gibraltar",
+    features: {
+      warpDrive: true,
+      slipSpace: false,
+    },
+  },
+
+  crew: {
+    capacity: 25,
+    captain: {
+      name: "Geland Frode",
+      age: 36,
+    },
+    navigator: {
+      name: "Johan Dermio",
+      age: 27,
+    },
+    engineer: {
+      name: "Relana Regara",
+      age: 25,
+    },
+  },
+
+  armament: {},
+};
