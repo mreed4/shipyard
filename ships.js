aac3f2c5335a4e6fee6f30f83852c4932a325a4a
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-04 19:53:06 -0700

    Further code readability improvements

--- ships.js
+++ ships.js
@@ -1,21 +1,27 @@
 const shipyard = ["Earth Orbit", "Mars Orbit", "Rings of Saturn", "Lagrange 2", "Europa", "Luna"];
 
 const shipClassInfo = [
+  // https://en.wikipedia.org/wiki/Ship_class
   {
+    // Retion
     shipClassName: "Retion",
     type: "Cruiser",
     displacement: 3000000,
-    crewCapacity: 25,
+    crewCapacity: 3025,
+    /*
+     */
     engines: {
       count: 6,
       make: "Medin Industries",
-      model: "Gibraltar",
+      model: "Gibraltar IV",
       features: {
         warpDrive: true,
         slipSpace: false,
-        inAtmosphere: true,
+        atmos: true,
       },
     },
+    /*
+     */
     armament: {},
   },
   "Varret",
@@ -31,8 +37,8 @@ class Ship {
 }
 
 const ship1 = {
-  name: "H.M.S. Ferdinand II",
-  shipClassInfo: shipClassInfo[0],
+  shipName: "H.M.S. Ferdinand II",
+  shipClassInfo: retion,
   shipyard: "Mars Orbit",
   yearBuilt: 2472,
   alignment: "EDF",
@@ -42,14 +48,19 @@ const ship1 = {
     captain: {
       name: "Geland Frode",
       age: 36,
+      gender: "f",
     },
     navigator: {
       name: "Johan Dermio",
       age: 27,
+      gender: "m",
     },
     engineer: {
       name: "Relana Regara",
       age: 25,
+      gender: "t",
     },
   },
 };
+
+console.log(retion === ship1.shipClassInfo);
