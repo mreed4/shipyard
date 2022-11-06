2191ccde50fc26aeccf4b7dacfad420502652c67
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-06 11:07:57 -0800

    Structuring changes

---
+++ data/classes/ship.js
@@ -0,0 +1,58 @@
+export class Ship {
+  constructor(shipName, shipClass, shipyard, yearBuilt, alignment) {
+    this._shipName = shipName;
+    this._shipClass = shipClass;
+    this._shipyard = shipyard;
+    this._yearBuilt = yearBuilt;
+    this._alignment = alignment;
+  }
+
+  get shipName() {
+    return this._shipName;
+  }
+
+  get shipClass() {
+    return this._shipClass;
+  }
+
+  get shipyard() {
+    return this._shipyard;
+  }
+
+  get yearBuilt() {
+    return this._yearBuilt;
+  }
+
+  get alignment() {
+    return this._alignment;
+  }
+
+  set shipName(string) {
+    this._shipName = string;
+  }
+
+  set shipClass(objectOrString) {
+    this._shipClass = objectOrString;
+  }
+
+  set shipyard(string) {
+    this._shipyard = string;
+  }
+
+  set yearBuilt(num) {
+    this._yearBuilt = num;
+  }
+
+  set alignment(string) {
+    this._alignment = string;
+  }
+
+  shipId() {
+    const isObject = typeof this._shipClass === "object";
+    const letter = isObject ? this._shipClass.name.slice(0, 2) : this._shipClass.slice(0, 2);
+    const min = 10000;
+    const max = 100000;
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return `${letter}${num}`;
+  }
+}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import { shipyards, earth, mars, rings, l2, europa, luna } from "./data/arrays/shipyards.js";
-import { shipClasses, retion, varrett, donbas, gesan, hyperion } from "./data/arrays/shipClasses.js";
+import { shipyards, earth, mars, rings, l2, europa, luna } from "./data/shipyards.js";
+import { shipClasses, retion, varrett, donbas, gesan, hyperion } from "./data/shipClasses.js";
+import { Ship } from "./data/classes/ship.js";
 
 function getRandInfo(info) {
   if (info === "class") {
@@ -13,65 +14,6 @@ function getRandInfo(info) {
   } else {
     const error = "ERROR Enter valid parameter";
     console.log(error);
-  }
-}
-
-class Ship {
-  constructor(shipName, shipClass, shipyard, yearBuilt, alignment) {
-    this._shipName = shipName;
-    this._shipClass = shipClass;
-    this._shipyard = shipyard;
-    this._yearBuilt = yearBuilt;
-    this._alignment = alignment;
-  }
-
-  get shipName() {
-    return this._shipName;
-  }
-
-  get shipClass() {
-    return this._shipClass;
-  }
-
-  get shipyard() {
-    return this._shipyard;
-  }
-
-  get yearBuilt() {
-    return this._yearBuilt;
-  }
-
-  get alignment() {
-    return this._alignment;
-  }
-
-  set shipName(string) {
-    this._shipName = string;
-  }
-
-  set shipClass(objectOrString) {
-    this._shipClass = objectOrString;
-  }
-
-  set shipyard(string) {
-    this._shipyard = string;
-  }
-
-  set yearBuilt(num) {
-    this._yearBuilt = num;
-  }
-
-  set alignment(string) {
-    this._alignment = string;
-  }
-
-  shipId() {
-    const isObject = typeof this._shipClass === "object";
-    const letter = isObject ? this._shipClass.name.slice(0, 2) : this._shipClass.slice(0, 2);
-    const min = 10000;
-    const max = 100000;
-    const num = Math.floor(Math.random() * (max - min)) + min;
-    return `${letter}${num}`;
   }
 }
 
@@ -164,5 +106,3 @@ function massProduceShips(desiredAmount, desiredClass, desiredShipyard) {
 
   return ships;
 }
-
-console.log(massProduceShips(25, "", rings));
