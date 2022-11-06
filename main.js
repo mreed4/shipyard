c5559bb4e3e4f9a323519620c0d318bb5506db2f
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-06 12:50:51 -0800

    Move a production function to its own file

--- main.js
+++ main.js
@@ -1,57 +1,5 @@
 import { shipyards, earth, mars, rings, l2, europa, luna } from "./data/arrays/shipyards.js";
 import { shipClasses, retion, varrett, donbas, gesan, hyperion } from "./data/arrays/shipClasses.js";
-import { Ship } from "./data/classes/ship.js";
-import { getRandInfo } from "./functions/helpers/getRandInfo.js";
+import { massProduceShips } from "./functions/massProduceShips.js";
 
-function massProduceShips(desiredAmount, desiredClass, desiredShipyard) {
-  const ships = [];
-
-  for (let i = 1; i <= desiredAmount; i++) {
-    let shipName = `Test${i}`;
-    let newShip;
-    /* 
-    
-    Randomizers
-     */
-    const randYearBuilt = getRandInfo("year");
-    const randShipClass = getRandInfo("class");
-    const randShipyard = getRandInfo("shipyard");
-    /* 
-
-    Pretty logic for conditional class creation
-     */
-    const classAndYard = desiredClass && desiredShipyard;
-    const classNoYard = desiredClass && !desiredShipyard;
-    const yardNoClass = !desiredClass && desiredShipyard;
-    /* 
-
-    Conditionally build class instances
-     */
-    if (classAndYard) {
-      newShip = new Ship(shipName, desiredClass, desiredShipyard, randYearBuilt, "USN");
-    } else if (yardNoClass) {
-      newShip = new Ship(shipName, randShipClass, desiredShipyard, randYearBuilt, "USN");
-    } else if (classNoYard) {
-      newShip = new Ship(shipName, desiredClass, randShipyard, randYearBuilt, "USN");
-    } else {
-      newShip = new Ship(shipName, randShipClass, randShipyard, randYearBuilt, "USN");
-    }
-    /* 
-
-    Use parts from generated class instances as items in an array
-    There will be a master array of all ships, and a sub-array per ship
-     */
-    const isObject = typeof newShip.shipClass === "object";
-    const shipId = newShip.shipId();
-    const shipClass = isObject ? newShip.shipClass.name : newShip.shipClass;
-    const shipType = isObject ? newShip.shipClass.type : undefined;
-    const shipShipyard = newShip.shipyard;
-    const shipYearBuilt = newShip.yearBuilt;
-
-    ships.push([/*shipName*/ shipId, shipClass /*shipType*/, shipShipyard /*shipYearBuilt*/, i]);
-  }
-
-  return ships;
-}
-
-console.log(massProduceShips(25, varrett, mars));
+console.log(massProduceShips());
