9051bf121686392cb6722ac59724bb1dd3114578
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-06 12:56:12 -0800

    Clean up main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import { shipyards, earth, mars, rings, l2, europa, luna } from "./data/arrays/shipyards.js";
-import { shipClasses, retion, varrett, donbas, gesan, hyperion } from "./data/arrays/shipClasses.js";
 import { massProduceShips } from "./functions/massProduceShips.js";
 
 console.log(massProduceShips());
