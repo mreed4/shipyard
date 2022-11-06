1413f1340a700223b0f8d8eca166400b42fe6f8e
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-06 12:42:08 -0800

    Set up for future success

--- data/classes/crewMember.js
+++ data/classes/crewMember.js
@@ -40,12 +40,13 @@ const ship0 = {
 };
 
 export class CrewMember {
-  constructor(name, age, gender, role, rating, scoringTRE) {
+  constructor(name, age, gender, rating, scoringTRE) {
     this._name = name;
     this._age = age;
     this._gender = gender;
-    this._role = role; // Captain, navigator, etc.
     this._rating = rating;
     this._scoringTRE = scoringTRE;
   }
+
+  /* Write getters/setters */
 }
