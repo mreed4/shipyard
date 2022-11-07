9419ad994169d53171f65f591f0266c15d8b004b
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-06 16:21:19 -0800

    Add engine serial randomizer and array builder

--- functions/helpers/getRandInfo.js
+++ functions/helpers/getRandInfo.js
@@ -1,7 +1,7 @@
 import { shipyards } from "../../data/arrays/shipyards.js";
 import { shipClasses } from "../../data/arrays/shipClasses.js";
 
-export function getRandInfo(info) {
+export function getRandInfo(info, num) {
   if (info === "class") {
     return shipClasses[Math.floor(Math.random() * shipClasses.length)];
   } else if (info === "shipyard") {
@@ -10,6 +10,14 @@ export function getRandInfo(info) {
     const min = 2300;
     const max = 2501;
     return Math.floor(Math.random() * (max - min)) + min;
+  } else if (info === "engine") {
+    const arr = [];
+    const min = 100000;
+    const max = 1000000;
+    for (let i = 1; i <= num; i++) {
+      arr.push(Math.floor(Math.random() * (max - min)) + min);
+    }
+    return arr;
   } else {
     const error = "ERROR Enter valid parameter";
     console.log(error);
