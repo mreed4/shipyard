65e46ed5fc1929639877a89f08e7212dc90b5593
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-29 21:16:00 -0800

    Add comments

--- data/arrays/shipClasses.js
+++ data/arrays/shipClasses.js
@@ -4,6 +4,8 @@ import { Hyperion } from "../classes/ships/Hyperion.js";
 import { Retion } from "../classes/ships/Retion.js";
 import { Varrett } from "../classes/ships/Varrett.js";
 
+// Used in an external randomizer to randomly return a ship class
 export const shipClasses = [Retion.getInfo(), Varrett.getInfo(), Donbas.getInfo(), Gesan.getInfo(), Hyperion.getInfo()];
 
+// Destructure to easily refernece each ship class
 export const [retion, varrett, donbas, gesan, hyperion] = shipClasses;
