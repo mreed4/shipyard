eb7b0684f4fb0a578823600e78ae3c673e246aad
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-30 19:19:21 -0800

    Logic seems to handle all possible inputs

--- functions/massProduceShips.js
+++ functions/massProduceShips.js
@@ -6,7 +6,17 @@ export function massProduceShips(desiredAmount = 5, includeTestname = false, des
 
   for (let i = 1; i <= desiredAmount; i++) {
     let shipName = `Test${i}`;
-    let newShip = new Ship(desiredClass, desiredShipyard);
+    let newShip;
+
+    if (!desiredClass && !desiredShipyard) {
+      newShip = new Ship(getRandInfo("class"), getRandInfo("shipyard"));
+    } else if (!desiredShipyard) {
+      newShip = new Ship(desiredClass, getRandInfo("shipyard"));
+    } else if (!desiredClass) {
+      newShip = new Ship(getRandInfo("class"), desiredShipyard);
+    } else {
+      newShip = new Ship(desiredClass, desiredShipyard);
+    }
 
     if (includeTestname) {
       ships.push([shipName, newShip]);
