cd04148d8a37c845014268e62d7101fea22b28b0
Matthew <78713317+mreed4@users.noreply.github.com>
2022-12-02 07:42:52 -0800

    Build out CrewMemebr logic and methods

--- data/classes/CrewMember.js
+++ data/classes/CrewMember.js
@@ -1,11 +1,38 @@
 export class CrewMember {
-  constructor(name, age, gender, rating, scoringTRE) {
-    this._name = name;
-    this._age = age;
-    this._gender = gender;
-    this._rating = rating;
-    this._scoringTRE = scoringTRE;
+  constructor() {
+    this.id = "(Will be calculated based on the below)";
+    this.name = "(Will be randomized based on gender and external arrays)";
+    this.birthplace = "(Will be randomized based on external array)";
+    this.age = this.generateAge();
+    this.gender = this.generateGender();
+    this.rating = this.generateRating();
+    this.scoreTRE = this.generateScoreTRE();
   }
 
-  /* Write getters/setters */
+  generateId(name) {}
+
+  generateGender() {
+    return Math.random() < 0.5 ? "f" : "m";
+  }
+
+  generateAge() {
+    const min = 20;
+    const max = 66;
+    const age = Math.floor(Math.random() * (max - min)) + min;
+    return age;
+  }
+
+  generateRating() {
+    const min = 1;
+    const max = 10;
+    const rating = Math.floor(Math.random() * (max - min)) + min;
+    return rating;
+  }
+
+  generateScoreTRE() {
+    const min = 2999;
+    const max = 4501;
+    const scoreTRE = Math.floor(Math.random() * (max - min)) + min;
+    return scoreTRE;
+  }
 }
