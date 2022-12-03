c2a5caece48bc0d5684706795a18388b0d0253ab
Matthew <78713317+mreed4@users.noreply.github.com>
2022-12-03 12:56:50 -0800

    Clean up formatting

--- data/classes/Ship.js
+++ data/classes/Ship.js
@@ -33,45 +33,54 @@ export class Ship {
     const prefix = String(this.shipClass.name).slice(0, 2);
     const suffix = String(this.shipyard).slice(0, 3).toUpperCase();
     const serial = Math.floor(Math.random() * (max - min)) + min;
-    return `${prefix}/${serial}/${suffix}`;
+
+    const shipId = `${prefix}/${serial}/${suffix}`;
+
+    return shipId;
   }
 
   generateEngineSerials() {
     const engineSerials = [];
     const min = 10000000000;
     const max = 100000000000;
+
     for (let i = 1; i <= this.shipClass.engines.count; i++) {
       const suffix = String.fromCharCode(i + 64);
       const serial = Math.floor(Math.random() * (max - min)) + min;
       engineSerials.push(`${serial}/${suffix}:`);
     }
+
     return engineSerials;
   }
 
   generateCrewMembers() {
-    const keyCrewRoles = Object.entries(this.shipClass.keyCrew);
+    const keyCrew = Object.entries(this.shipClass.keyCrew);
     let crewMembers = []; // Converted to object at end
     const subCrewMembers = [];
 
     /*
     The below code will instantiate a new CrewMember at every value in the object 
     */
-    keyCrewRoles.forEach((crewMember) => {
-      const crewMemberRole = crewMember[0];
-      const crewMemberName = crewMember[1];
-      const isString = typeof crewMemberName === "string";
+    keyCrew.forEach((crewMember) => {
+      const key = crewMember[0];
+      const value = crewMember[1];
+      const isString = typeof value === "string";
+
       if (isString) {
-        crewMembers.push([crewMemberRole, new CrewMember()]);
+        crewMembers.push([key, new CrewMember()]);
       } else {
-        const blankSubCrewMembers = Object.entries(crewMemberName);
-        blankSubCrewMembers.forEach((subCrewMember) => {
-          const subCrewMemberRole = subCrewMember[0];
-          subCrewMembers.push([subCrewMemberRole, new CrewMember()]);
+        const subKeyCrew = Object.entries(value);
+
+        subKeyCrew.forEach((subCrewMember) => {
+          const subKey = subCrewMember[0];
+          subCrewMembers.push([subKey, new CrewMember()]);
         });
-        crewMembers.push([crewMemberRole, Object.fromEntries(subCrewMembers)]);
+
+        crewMembers.push([key, Object.fromEntries(subCrewMembers)]);
       }
     });
 
+    // Convert array back to object
     crewMembers = Object.fromEntries(crewMembers);
 
     return crewMembers;
