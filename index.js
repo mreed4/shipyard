cb9a5c2a2aba882c8f9927d1fcadb99ea6c42599
Matthew <78713317+mreed4@users.noreply.github.com>
2022-12-02 20:33:44 -0800

    Update links

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-import { retion, varrett, donbas, gesan, hyperion, letios } from "./data/arrays/shipClasses.js";
-import { earth, mars, rings, l2, europa, luna, shipyards } from "./data/arrays/shipyards.js";
+import { retion, varrett, donbas, gesan, hyperion, letios } from "./data/arrays/ship/shipClasses.js";
+import { earth, mars, rings, l2, europa, luna, shipyards } from "./data/arrays/ship/shipyards.js";
 import { massProduceShips } from "./functions/massProduceShips.js";
 import { Ship } from "./data/classes/Ship.js";
 
-let ships = massProduceShips(10, false, "", luna);
+let ships = massProduceShips(5, gesan);
 
-let basicData = ships.map((ship) => {
-  let shipName;
-  let shipClass;
-  let shipyard;
-  let info;
-  if (ship.length === 2) {
-    // Length will be 2 if second argument in massProduceShips is "true"
-    shipName = ship[0];
-    shipClass = ship[1].shipClass.name;
-    shipyard = ship[1].shipyard;
-    info = [{ shipName }, { shipClass }, { shipyard }];
-  } else {
-    shipClass = ship.shipClass.name;
-    shipyard = ship.shipyard;
-    info = [{ shipClass }, { shipyard }];
-  }
-
-  return info;
-});
-
-let varrettOnly = basicData.filter((ship) => ship[0].shipClass === "Varrett");
-
-let crewMembers = ships.map((ship) => ship.crewMembers);
+let crewMembers = ships.map((ship) => ship.crewMembers.deckMasters);
 
 let shipIds = ships.map((ship) => ship.shipId);
 
-console.log(shipIds);
+console.log(crewMembers);
