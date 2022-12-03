1095c36d1c1338220c79bc4bd80be624c227afad
Matthew <78713317+mreed4@users.noreply.github.com>
2022-12-02 20:04:16 -0800

    Clean up methods

--- data/classes/Ship.js
+++ data/classes/Ship.js
@@ -5,9 +5,9 @@ export class Ship {
     this.shipClass = shipClass;
     this.shipyard = shipyard;
     // Everything below in the constructor is unique per ship
-    this.shipId = this.generateShipId(this.shipClass.name, this.shipyard);
-    this.engineSerials = this.generateEngineSerials(this.shipClass.engines.count);
-    this.crewMembers = this.generateCrewMembers(this.shipClass.keyCrew);
+    this.shipId = this.generateShipId();
+    this.engineSerials = this.generateEngineSerials();
+    this.crewMembers = this.generateCrewMembers();
     this.mods = [];
     this.__gameData = {
       moddedHitPoints: 0,
@@ -27,20 +27,20 @@ export class Ship {
     this.yearBuilt = Number(desiredYearBuilt);
   }
 
-  generateShipId(shipClassName, shipShipyard) {
+  generateShipId() {
     const min = 1000000000;
     const max = 10000000000;
-    const prefix = String(shipClassName).slice(0, 2);
-    const suffix = String(shipShipyard).slice(0, 3).toUpperCase();
+    const prefix = String(this.shipClass.name).slice(0, 2);
+    const suffix = String(this.shipyard).slice(0, 3).toUpperCase();
     const serial = Math.floor(Math.random() * (max - min)) + min;
     return `${prefix}/${serial}/${suffix}`;
   }
 
-  generateEngineSerials(numEngines) {
+  generateEngineSerials() {
     let engineSerials = [];
     const min = 10000000000;
     const max = 100000000000;
-    for (let i = 1; i <= numEngines; i++) {
+    for (let i = 1; i <= this.shipClass.engines.count; i++) {
       const suffix = String.fromCharCode(i + 64);
       const serial = Math.floor(Math.random() * (max - min)) + min;
       engineSerials.push(`${serial}/${suffix}:`);
@@ -48,8 +48,8 @@ export class Ship {
     return engineSerials;
   }
 
-  generateCrewMembers(keyCrew) {
-    let keyCrewRoles = Object.entries(keyCrew);
+  generateCrewMembers() {
+    let keyCrewRoles = Object.entries(this.shipClass.keyCrew);
     let crewMembers = [];
     let subCrewMembers = [];
 
