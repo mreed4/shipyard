a57d5ec662d260d0432358f5dea3771ed6fc1866
Matthew <78713317+mreed4@users.noreply.github.com>
2022-12-04 09:41:58 -0800

    Grammar

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { retion, varrett, donbas, gesan, hyperion, letios } from "./data/arrays/ship/shipClasses.js";
 import { earth, mars, rings, l2, europa, luna, shipyards } from "./data/arrays/ship/shipyards.js";
 import { massProduceShips } from "./functions/massProduceShips.js";
+import { generateCrewMembers } from "./functions/getCrewInfo.js";
 import { Ship } from "./data/classes/Ship.js";
+import { CrewMember } from "./data/classes/CrewMember.js";
+import { getRandInfo } from "./functions/helpers/getRandInfo.js";
 
-let ships = massProduceShips(5, gesan);
-
-let crewMembers = ships.map((ship) => ship.crewMembers.deckMasters);
-
-let shipIds = ships.map((ship) => ship.shipId);
+let ships = massProduceShips(30, gesan);
+let crewMembers = generateCrewMembers(25);
 
 console.log(crewMembers);
